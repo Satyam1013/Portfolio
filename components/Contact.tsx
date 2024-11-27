73e0d57c7c00c6a0ff8f0329a811205ef1d1a834
Satyam Banwale <satyambanwale786@gmail.com>
2024-11-27 16:11:29 +0530

    fixed contact card width

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -59,7 +59,7 @@ export default function Contact() {
         </h2>
 
         {/* Responsive Contact Info Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+        <div className="w-[90%] m-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
           {[
             {
               icon: <FaMap className="text-designColor" />,
